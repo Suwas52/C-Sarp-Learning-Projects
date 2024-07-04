fba2f38013be1ed38737f2bffada446353ef5483
Suwas52 <suwasdanuwar18@gmail.com>
2024-07-04 21:31:29 +0545

    first project using c#

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = 1;
+            Console.WriteLine(data);
+
+            var sum = 10;
+            sum = sum + data;
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Hello word");
+            Console.ReadLine();
+        }
+    }
+}
