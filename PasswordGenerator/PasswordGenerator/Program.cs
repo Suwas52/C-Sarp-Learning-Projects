9346b7b753c8f5d30b133e8e5b07a1d74f79e7b3
Suwas52 <suwasdanuwar18@gmail.com>
2024-07-21 19:07:27 +0545

    add password generator projects

---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter the length you want to generate: ");
+
+            string InputLength = Console.ReadLine();
+
+            int passwordLength = Convert.ToInt32(InputLength);
+
+
+
+            Random rnd = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZaabcdefghijklmnopqrstuvwxyz0123456789@#$%&";
+
+            string genPassword = "";
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int rndDigit = rnd.Next(0, chars.Length-1);
+                var genChar = chars[rndDigit];
+                genPassword += genChar;
+                
+            }
+
+            /*Console.WriteLine(rnd.Next());*/
+
+            Console.WriteLine(genPassword);
+
+
+            Console.ReadLine();
+        }
+    }
+}
